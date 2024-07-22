01339a506a5124f6e672778d19649e8eb41a4fa3
OES-MAIN-PC\hhagi <h.hagiwara@ha.hocoroco.ne.jp>
2024-07-22 12:20:15 +0900

    IDEMPIERE-6159:Add drop ship location to ITaxLookup

--- src/jpiere/plugin/simpleinputwindow/callout/SIWCalloutInvoiceTax.java
+++ src/jpiere/plugin/simpleinputwindow/callout/SIWCalloutInvoiceTax.java
@@ -16,7 +16,11 @@ package jpiere.plugin.simpleinputwindow.callout;
 import java.sql.Timestamp;
 import java.util.Properties;
 
-import org.compiere.model.Tax;
+import org.adempiere.base.Core;
+import org.compiere.model.GridTab;
+import org.compiere.model.MInOutLine;
+import org.compiere.model.MOrder;
+import org.compiere.model.MOrderLine;
 import org.compiere.util.Env;
 
 import jpiere.plugin.simpleinputwindow.base.ISimpleInputWindowCallout;
@@ -32,8 +36,9 @@ public class SIWCalloutInvoiceTax implements ISimpleInputWindowCallout {
 			return "";
 
 		int WindowNo = dataBinder.getSimpleInputWindow().getGridTab().getWindowNo();
+		GridTab mTab = dataBinder.getSimpleInputWindow().getGridTab();
 		Properties ctx = Env.getCtx();
-		int tabNo = dataBinder.getSimpleInputWindow().getGridTab().getTabNo();
+		int tabNo = mTab.getTabNo();
 
 		//	Check Product
 		int M_Product_ID = 0;
@@ -60,12 +65,13 @@ public class SIWCalloutInvoiceTax implements ISimpleInputWindowCallout {
 		Timestamp billDate = Env.getContextAsDate(ctx, WindowNo, "DateInvoiced");
 
 		int AD_Org_ID = Env.getContextAsInt(ctx, WindowNo, "AD_Org_ID");
-
-		//
-		int C_Tax_ID = Tax.get (ctx, M_Product_ID, C_Charge_ID, billDate, billDate,
-			AD_Org_ID, 0, shipC_BPartner_Location_ID, shipC_BPartner_Location_ID,
-			"Y".equals(Env.getContext(ctx, WindowNo, "IsSOTrx")), null);
-
+		
+		String deliveryViaRule = getLineDeliveryViaRule(ctx, WindowNo, mTab);
+		int dropshipLocationId = getDropShipLocationId(ctx, WindowNo, mTab);
+		int C_Tax_ID = Core.getTaxLookup().get(ctx, M_Product_ID, C_Charge_ID, billDate, billDate,
+			AD_Org_ID, 0, shipC_BPartner_Location_ID, shipC_BPartner_Location_ID,dropshipLocationId,
+			"Y".equals(Env.getContext(ctx, WindowNo, "IsSOTrx")), deliveryViaRule, null);
+		
 		if (C_Tax_ID == 0)
 			;
 		else
@@ -76,8 +82,51 @@ public class SIWCalloutInvoiceTax implements ISimpleInputWindowCallout {
 		return "";
 	}	//	tax
 
+	private String getLineDeliveryViaRule(Properties ctx, int windowNo, GridTab mTab) {
+		if (mTab.getValue("C_OrderLine_ID") != null) {
+			int C_OrderLine_ID = (Integer) mTab.getValue("C_OrderLine_ID");
+			if (C_OrderLine_ID > 0) {
+				MOrderLine orderLine = new MOrderLine(ctx, C_OrderLine_ID, null);
+				return orderLine.getParent().getDeliveryViaRule();
+			}
+		}
+		if (mTab.getValue("M_InOutLine_ID") != null) {
+			int M_InOutLine_ID = (Integer) mTab.getValue("M_InOutLine_ID");
+			if (M_InOutLine_ID > 0) {
+				MInOutLine ioLine = new MInOutLine(ctx, M_InOutLine_ID, null);
+				return ioLine.getParent().getDeliveryViaRule();
+			}
+		}
+		int C_Order_ID = Env.getContextAsInt(ctx, windowNo, "C_Order_ID", true);
+		if (C_Order_ID > 0) {
+			MOrder order = new MOrder(ctx, C_Order_ID, null);
+			return order.getDeliveryViaRule();
+		}
+		return null;
+	}
 
-
+	/**
+	 * Get the drop shipment location ID from the related order
+	 * @param ctx
+	 * @param windowNo
+	 * @param mTab
+	 * @return
+	 */
+	private int getDropShipLocationId(Properties ctx, int windowNo, GridTab mTab) {
+		if (mTab.getValue("C_OrderLine_ID") != null) {
+			int C_OrderLine_ID = (Integer) mTab.getValue("C_OrderLine_ID");
+			if (C_OrderLine_ID > 0) {
+				MOrderLine orderLine = new MOrderLine(ctx, C_OrderLine_ID, null);
+				return orderLine.getParent().getDropShip_Location_ID();
+			}
+		}
+		int C_Order_ID = Env.getContextAsInt(ctx, windowNo, "C_Order_ID", true);
+		if (C_Order_ID > 0) {
+			MOrder order = new MOrder(ctx, C_Order_ID, null);
+			return order.getDropShip_Location_ID();
+		}
+		return -1;
+	}
 
 
 }
